70e8d7d3696fee83566a2d5f1abda3662c7f063b
udit-sisodia <uditsisodia1234@gmail.com>
2026-02-04 00:42:05 +0530

    created hero section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './components/Navbar'
+import HeroSection from './sections/HeroSection'
+
+const App = () => {
+  return (
+    <main>
+      <NavBar/>
+      <HeroSection/>
+    </main>
+  )
+}
+
+export default App
