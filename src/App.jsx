f629b144c7bb98fb1ad99d630ebd0a134c616b70
udit-sisodia <uditsisodia1234@gmail.com>
2026-02-04 01:16:04 +0530

    created message section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import NavBar from './components/Navbar'
 import HeroSection from './sections/HeroSection'
+import gsap from 'gsap';
+import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
+import MessageSection from './sections/MessageSection';
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
   return (
     <main>
       <NavBar/>
       <HeroSection/>
+      <MessageSection/>
+      <div className='h-dvh'></div>
     </main>
   )
 }
