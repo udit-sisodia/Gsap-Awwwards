8f4259a922521c92099c73779359918a03c3bcf6
udit-sisodia <uditsisodia1234@gmail.com>
2026-02-04 18:19:05 +0530

    added testimonial section and also added pinning multiple elements

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import FlavorSection from './sections/FlavorSection';
 import { useGSAP } from '@gsap/react';
 import NutritionSection from './sections/NutritionSection';
 import BenefitSection from './sections/BenefitSection';
+import TestimonialSection from './sections/TestimonialSection';
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
@@ -27,7 +28,11 @@ const App = () => {
       <MessageSection/>
       <FlavorSection/>
       <NutritionSection/>
+      <div>
       <BenefitSection/>
+      <TestimonialSection/>
+      </div>
+      
       <div className='h-dvh'></div>
       </div>
       </div>
