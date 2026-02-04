2903a346bcb7a4dd8faf928947d85f8f193583dc
udit-sisodia <uditsisodia1234@gmail.com>
2026-02-04 18:36:29 +0530

    corrected some bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from './components/Navbar'
+import NavBar from './components/NavBar'
 import HeroSection from './sections/HeroSection'
 import gsap from 'gsap';
 import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
