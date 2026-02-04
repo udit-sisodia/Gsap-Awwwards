08b8752d9345314f6bfce34f85db777f866e8b53
udit-sisodia <uditsisodia1234@gmail.com>
2026-02-04 15:51:01 +0530

    used smooth scrolling plugin

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,28 @@ import gsap from 'gsap';
 import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
 import MessageSection from './sections/MessageSection';
 import FlavorSection from './sections/FlavorSection';
+import { useGSAP } from '@gsap/react';
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
   return (
     <main>
       <NavBar/>
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
       <HeroSection/>
       <MessageSection/>
       <FlavorSection/>
       <div className='h-dvh'></div>
+      </div>
+      </div>
     </main>
   )
 }
